14fa63dffaa408b5b5222e41f380e65e62b92fb1
Alex Maystrenko <alexeytech@gmail.com>
2019-03-04 23:42:00 +0100

    fix dbus-launch

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,5 @@ export QT_QPA_PLATFORM=wayland-egl
 export GDK_BACKEND=wayland
 export CLUTTER_BACKEND=wayland
 export SDL_VIDEODRIVER=wayland
-export $(dbus-launch)
 winpr-makecert -rdp secret
-exec weston --backend=rdp-backend.so --rdp4-key=secret.key --width=1920 --height=1080
+exec dbus-launch --exit-with-session weston --backend=rdp-backend.so --rdp4-key=secret.key --width=1920 --height=1080
