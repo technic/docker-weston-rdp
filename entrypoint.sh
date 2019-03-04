fb8578271b14a113ecd9d27adbf67795022cc767
Alex Maystrenko <alexeytech@gmail.com>
2019-03-05 00:05:30 +0100

    Fix set locale

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ export QT_QPA_PLATFORM=wayland-egl
 export GDK_BACKEND=wayland
 export CLUTTER_BACKEND=wayland
 export SDL_VIDEODRIVER=wayland
+export LC_ALL=en_US.UTF-8
 winpr-makecert -rdp secret
-exec dbus-launch --exit-with-session weston --backend=rdp-backend.so --rdp4-key=secret.key --width=1920 --height=1080
+exec dbus-launch weston --backend=rdp-backend.so --rdp4-key=secret.key --width=1920 --height=1080
